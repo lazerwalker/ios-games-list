b28dd5155c6a7dc6bb4702c1e3f7a320c68dd50e
Mike Walker <michael@lazerwalker.com>
2016-01-12 12:27:18 -0800

    Add platform switch

--- filter.js
+++ filter.js
@@ -2,9 +2,24 @@ function toArray(nodeList) {
   return Array.prototype.slice.call(nodeList)
 }
 
+toArray(document.querySelectorAll("input.filter"))
+  .forEach(function(el) {
+    el.addEventListener("change", updateFilters)
+  })
+
+toArray(document.querySelectorAll("input.platform"))
+  .forEach(function(el) {
+    el.addEventListener("change", updatePlatform)
+  })
+
+
+//---
+// Filters
+//---
+
 // Returns an Array of string IDs
 function getCheckedFilters() {
-  return toArray(document.getElementsByTagName("input"))
+  return toArray(document.querySelectorAll("input.filter"))
     .filter(function(input) { return input.checked })
     .map(function(input) { return input.id });
 }
@@ -25,12 +40,35 @@ function classSelectorForFilters(filters) {
     .join(",");
 }
 
-function update() {
+function updateFilters(e) {
   hideAllGames();
   show(classSelectorForFilters(getCheckedFilters()));
+
+  if (e) updatePlatform()
 }
 
-toArray(document.getElementsByTagName("input"))
-  .forEach(function(el) {
-    el.addEventListener("change", update)
-  })
+//---
+// Platform
+//---
+
+function updatePlatform(e) {
+  if (e) updateFilters();
+  
+  var iphone = document.getElementById("iphone").checked
+  var ipad = document.getElementById("ipad").checked
+
+  if (!iphone) {
+    toArray(document.querySelectorAll(".iphone"))
+      .forEach(function(el) { el.style.display = "none" });    
+  }
+
+  if (!ipad) {
+    toArray(document.querySelectorAll(".ipad"))
+      .forEach(function(el) { el.style.display = "none" });    
+  }
+
+  if (!iphone && !ipad) {
+    toArray(document.querySelectorAll(".both-platforms"))
+      .forEach(function(el) { el.style.display = "none" });    
+  }
+}
